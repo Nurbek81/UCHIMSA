593ef05ae58b80fed7f6459fec3f754fd965e283
Nurbek <nurbekkolbaev81@gmail.com>
2022-11-03 21:35:36 +0600

    summa

---
+++ Ex003/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int numberA = 31;
+int numberВ = 5;
+int result = numberA + numberВ;
+Console.WriteLine(result);
+
